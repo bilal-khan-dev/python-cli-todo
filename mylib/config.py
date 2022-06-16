2e82737b5388a4de244dc59f15bda9fab55197fa
Bilal Khan <38106396+devbilalkhan@users.noreply.github.com>
2022-06-16 10:39:20 +0000

    Initialize config  file

--- mylib/config.py
+++ mylib/config.py
@@ -23,12 +23,24 @@ __version__ = "0.1.1"
 CONFIG_DIR_PATH = Path(typer.get_app_dir(__app_name__))
 CONFIG_FILE_PATH = CONFIG_DIR_PATH / "config.ini"
 
+
 def init_app() -> int:
-    """ Initialize the config file"""
-    
+    """Initialize the app"""
     config_code = _init_config_file()
-    if config_code != SUCCESS
+    if config_code != SUCCESS:
         return config_code
     return SUCCESS
 
 
+def _init_config_file() -> int:
+    """Initialize the config file"""
+    try:
+        CONFIG_DIR_PATH.mkdir(exist_ok=True)
+    except OSError:
+        return DIR_ERROR
+    return SUCCESS
+
+    try:
+        CONFIG_FILE_PATH.touch(exist_ok=True)
+    except OSError:
+        return FILE_ERROR
